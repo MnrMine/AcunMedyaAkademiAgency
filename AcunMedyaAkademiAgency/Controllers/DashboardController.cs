239ab373a4a96e42fd1ed96e35e628549b25146f
MnrMine <mnr_mine_agri@hotmail.com>
2025-02-20 22:19:20 +0300

    UI_Project_add

---
+++ AcunMedyaAkademiAgency/Controllers/DashboardController.cs
@@ -0,0 +1,24 @@
+﻿using AcunMedyaAkademiAgency.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcunMedyaAkademiAgency.Controllers
+{
+    
+    public class DashboardController : Controller
+    {
+        AgencyContext context = new AgencyContext();
+        public ActionResult Index()
+        {
+            ViewBag.bildirimsayisi = context.Notifications.ToList().Count();
+            ViewBag.projectsayisi = context.Projects.ToList().Count();
+            int categorygrafik = context.Categories.Where(x => x.CategoryName == "Grafik Tasarım").Select(y => y.CategoryId).FirstOrDefault();
+            ViewBag.grafikCount = context.Projects.Where(x => x.CategoryId == categorygrafik).Count();
+            ViewBag.personelsayisi = context.Teams.ToList().Count();
+            return View();
+        }
+    }
+}
---
+++ AcunMedyaAkademiAgency/Controllers/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿using AcunMedyaAkademiAgency.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcunMedyaAkademiAgency.Controllers
+{
+    public class DefaultController : Controller
+    {
+        AgencyContext context=new AgencyContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult ProjectPartial()
+        {
+            var values = context.Projects.OrderByDescending(x => x.ProjectId).Take(6).ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult ModalPartial()
+        {
+            var values = context.ProjectDetails.ToList();
+            return PartialView(values);
+        }
+    }
+}
